7e8e287c7bc19f1a88c98d13e08cb7a644afabd4
Gregori-Rivera <gregori.rivera@gmail.com>
2022-11-08 01:27:25 +0000

    creada las excusas

--- src/app.js
+++ src/app.js
@@ -5,34 +5,9 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-// window.onload = function() {
-//   //write your code here
-//   console.log("Hello Rigo from the console!");
-
-//   let who = ["the dog", "my granma", "his turtle", "my bird"];
-//   let what = ["eat", "pissed", "crushed", "broked"];
-//   let when = [
-//     "before the class",
-//     "right in time",
-//     "when I finished",
-//     "during my lunch",
-//     "while I was praying"
-//   ];
-
-//   let excusa = document.getElementById("excuse");
-//   excusa.innerHTML = "Ahora si";
-// };
-
-let who = ["The dog", "My grandma", "His turtle", "My bird"];
-let action = ["ate", "peed", "crushed", "broke"];
-let what = ["my homework", "the keys", "the car"];
-let when = [
-  "before the class",
-  "right on time",
-  "when I finished",
-  "during my lunch",
-  "while I was praying"
-];
+let who = ["Mi vecino", "Mi Comadre", "Su Perro", "Mi Loro"];
+let action = ["se comio", "perdió", "encontró"];
+let what = ["las llaves", "la torta", "la cartera"];
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
@@ -43,5 +18,5 @@ window.onload = function() {
   excusa.innerHTML = `${who[getRandomInt(who.length)]} 
   ${action[getRandomInt(action.length)]} 
   ${what[getRandomInt(what.length)]} 
-  ${when[getRandomInt(when.length)]}`;
+`;
 };
